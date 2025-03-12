d2ff199e4588d85950a492cffdd44718642441cf
jordankageorgieva <jo_georgieva@abv.bg>
2025-03-12 10:22:42 +0200

    links fixed to work

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,45 @@
 import './App.css';
+
 import CataloguePage from './components/catalogue-page/CataloguePage';
 import CreatePage from './components/create-page/CreatePage';
 import DetailsPage from './components/details-page/DetailsPage';
-import EditPage from './components/edit-page/EditPage';
+import GameEdit from './components/game-edit/GameEdit';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import LoginPage from './components/login-page/LoginPage';
 import RegisterPage from './components/register-page/RegisterPage';
+import { Routes, Route } from 'react-router';
 
 function App() {
 
   return (
     <>
       <body>
-        <div id="box"></div>
+        <div id="box">
 
-        <Header />
 
-        {/* <!-- Main Content --> */}
-        <main id="main-content">
-        </main>
-        <Home />
-        <LoginPage />
-        <RegisterPage />
-        <CreatePage />
-        <EditPage />
-        <DetailsPage />
-        <CataloguePage />
 
+          <Header />
+
+          {/* <!-- Main Content --> */}
+          <main id="main-content">
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/login' element={<LoginPage />} />
+              <Route path='/register' element={<RegisterPage />} />
+              <Route path='/games/create' element={<CreatePage />} />
+              <Route path='/games' element={<CataloguePage />} />
+
+            </Routes>
+          </main>
+
+         
+          
+          
+          {/* <GameEdit />
+          <DetailsPage /> */}
+          
+        </div>
       </body>
     </>
   )
--- client/src/components/game-edit/GameEdit.jsx
+++ client/src/components/game-edit/GameEdit.jsx
@@ -1,4 +1,4 @@
-export default function EditPage() {
+export default function GameEdit() {
     return (
         <>
             {/* <!-- Edit Page ( Only for the creator )--> */}
