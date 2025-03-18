1c886072b49ed30cd8f453e8b3359dd5de0e69bc
jordankageorgieva <jo_georgieva@abv.bg>
2025-03-18 18:49:26 +0200

    submit form is ACTION

--- client/src/components/login-page/LoginPage.jsx
+++ client/src/components/login-page/LoginPage.jsx
@@ -1,3 +1,4 @@
+import { useActionState } from "react";
 import { Link, useNavigate } from "react-router";
 
 export default function LoginPage({
@@ -5,27 +6,32 @@ export default function LoginPage({
 }) {
     const navigate = useNavigate();
 
-    const loginAction = (event) => {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        const email = formData.get('email');
-        login(email);
+    const loginHandler = (previusstate, formData) => {
+        console.log("formData " + formData);
+     
+        const state = Object.fromEntries(formData);
+        console.log(state);
+
+        login(state.email);
 
         navigate("/games");
-
     }
+
+    const [state, loginAction, isPending] = useActionState(loginHandler, {email: '', password: ''});
+    
+    
     return (
         <>
             {/* <!-- Login Page ( Only for Guest users ) --> */}
             <section id="login-page" className="auth">
-            <form id="login" onSubmit={loginAction}>
+            <form id="login" action={loginAction}>
                 <div className="container">
                     <div className="brand-logo"></div>
                     <h1>Login</h1>
                     <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
+                    <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" autoComplete="email"/>
                     <label htmlFor="login-pass">Password:</label>
-                    <input type="password" id="login-password" name="password" fdprocessedid = "45q4he" />
+                    <input type="password" id="login-password" name="password" fdprocessedid = "45q4he" autoComplete="password" />
                     <input type="submit" className="btn submit" value="Login" />
                     <p className="field">
                         <span>If you don't have profile click <Link to="/register">here</Link></span>
